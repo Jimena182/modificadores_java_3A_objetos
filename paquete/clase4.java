245d57bad9edba83ae30c6ad362b87062c70a744
Jimena182 <baploru@gmail.com>
2025-06-25 12:50:02 -0600

    Initial local commit

---
+++ paquete/clase4.java
@@ -0,0 +1,19 @@
+package paquete;
+
+public class clase4 {
+    private boolean x;
+
+    public boolean isx(){
+        return x;
+    }
+
+    public boolean isX() {
+        return x;
+    }
+
+    public void setX(boolean x) {
+        this.x = x;
+    }
+
+    
+}
