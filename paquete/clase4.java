0d72e0c4543324a8fe410251a30810cfa4e6a5cb
Jimena182 <baploru@gmail.com>
2025-06-27 11:06:54 -0600

    Add y in clase4.java

--- paquete/clase4.java
+++ paquete/clase4.java
@@ -2,18 +2,32 @@ package paquete;
 
 public class clase4 {
     private boolean x;
+    private double y;
 
-    public boolean isx(){
-        return x;
-    }
-
-    public boolean isX() {
+    public boolean getx(){
         return x;
     }
 
     public void setX(boolean x) {
         this.x = x;
     }
+
+    public double isY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    @Override
+    public String toString() {
+        return "clase4 [x=" + x + ", y=" + y + "]";
+    }
+
+    
+
+    
 
     
 }
